c6df1d489d2789b241d41d7b173e31e1be410d7c
leocodeio <basicleo787@gmail.com>
2025-03-31 10:26:27 +0530

    perf(Allowed hosts added): Allowed hosts added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    allowedHosts: ["chatpilot.leocode.tech", "localhost", "127.0.0.1"],
+  },
+});
